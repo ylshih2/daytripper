49e3defa4965aad5f3d659af99922fb197eeec78
Scott Shih <rockstone7000@gmail.com>
2025-07-05 20:19:03 +0800

    try cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  adapter: netlify(),
 });
